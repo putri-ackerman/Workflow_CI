9bce2ca54c5f867d891159f87b4e353a6caf981f
Putri Amallia Jatmika <133171307+putri-ackerman@users.noreply.github.com>
2026-01-05 22:04:42 +0700

    Add files via upload

---
+++ MLProject/modelling.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import root_mean_squared_error
+import mlflow
+import mlflow.sklearn
+import os
+
+# Menghubungkan ke DagsHub secara otomatis di GitHub Actions
+mlflow.set_tracking_uri("https://dagshub.com/putri_ackerman/my-first-repo.mlflow")
+
+# load data
+df = pd.read_csv('home_price_precprocessing/house_price_clean.csv')
+X = df.drop('House_Price', axis=1) 
+y = df['House_Price']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# activate autolog
+mlflow.sklearn.autolog()
+
+# training model
+rf_model = RandomForestRegressor(random_state=42)
+
+with mlflow.start_run(run_name="Random Forest Basic"):
+    rf_model.fit(X_train, y_train)
+    
+    # evaluasi sederhana
+    y_pred = rf_model.predict(X_test)
+    rmse = root_mean_squared_error(y_test, y_pred)
